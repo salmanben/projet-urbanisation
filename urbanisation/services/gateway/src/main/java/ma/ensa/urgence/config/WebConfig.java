b93eed2c3d35d9de388a059f483cdcc829519b83
salmanben <salmanbenomar250@gmail.com>
2024-12-07 14:03:05 +0100

    Fix issues

---
+++ urbanisation/services/gateway/src/main/java/ma/ensa/urgence/config/WebConfig.java
@@ -0,0 +1,23 @@
+package ma.ensa.urgence.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
+/* 
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow CORS for all endpoints
+                .allowedOrigins("http://localhost:4200") // Allow requests from this origin
+                .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "PUT") // Allow specific HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+    }
+
+}*/
